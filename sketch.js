425ef1e8acd85a50d3cdfd7b9f43ec273251adc6
Mayla Rocha <maylaetb@gmail.com>
2023-01-19 19:18:52 -0300

    criando e movimentado atores

--- sketch.js
+++ sketch.js
@@ -1,7 +1,55 @@
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemDoCarro;
+
+//ator
+let xAtor = 100;
+let yAtor = 366;
+let larguraAtor = 30;
+let alturaAtor = 30;
+
+//carro
+let xCarro = 400;
+let yCarro = 40;
+let larguraCarro = 70;
+let alturaCarro = 40;
+
+
+function preload(){
+  imagemDaEstrada = loadImage("material/estrada.png")
+  imagemDoAtor = loadImage("material/ator-1.png")
+  imagemDoCarro = loadImage("material/carro-1.png")
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(220);
+  background(imagemDaEstrada);
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro();
+  movimentaAtor();
+}
+
+function mostraAtor(){
+  image(imagemDoAtor, xAtor, yAtor, larguraAtor, alturaAtor);
+}
+
+function mostraCarro(){
+  image(imagemDoCarro, xCarro, yCarro, larguraCarro, alturaCarro);
+}  
+
+function movimentaCarro(){
+  xCarro -= 2;
+}
+
+function movimentaAtor(){
+  if (keyIsDown(UP_ARROW)){
+    yAtor -= 3;
+  }
+  if (keyIsDown(DOWN_ARROW)){
+    yAtor += 3;
+  }
 }
