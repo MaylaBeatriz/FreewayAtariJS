26ca784411d89dc05ee79f144b01d728c4e0c4be
Mayla Rocha <maylaetb@gmail.com>
2023-01-20 17:46:17 -0300

    criacao variavel velocidadeCarro

--- carro.js
+++ carro.js
@@ -3,18 +3,22 @@ let xCarro1 = 700;
 let yCarro1 = 40;
 let larguraCarro1 = 70;
 let alturaCarro1 = 40;
+let velocidadeCarro1 = 2;
 
 //carro 2
 let xCarro2 = 10;
 let yCarro2 = 95;
 let larguraCarro2 = 70;
 let alturaCarro2 = 40;
+let velocidadeCarro2 = 2.5;
 
 //carro 3
 let xCarro3 = 800;
 let yCarro3 = 150;
 let larguraCarro3 = 70;
 let alturaCarro3 = 40;
+let velocidadeCarro3 = 3.2;
+
 
 function mostraCarro(){
     image(imagemDoCarro1, xCarro1, yCarro1, larguraCarro1, alturaCarro1);
@@ -23,7 +27,7 @@ function mostraCarro(){
 }  
   
 function movimentaCarro(){
-    xCarro1 -= 2;
-    xCarro2 += 2;
-    xCarro3 -= 2;
+    xCarro1 -= velocidadeCarro1;
+    xCarro2 += velocidadeCarro2;
+    xCarro3 -= velocidadeCarro3;
 }
