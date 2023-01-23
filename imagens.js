7d4a9e5821a876a38b3102c302c6d0c70b88656b
Mayla Rocha <maylaetb@gmail.com>
2023-01-23 15:10:46 -0300

    organizando pasta imagens

--- imagens.js
+++ imagens.js
@@ -6,10 +6,10 @@ let imagemDoCarro2;
 let imagemDoCarro3;
 
 function preload() {
-    imagemDaEstrada = loadImage("material/estrada.png");
-    imagemDoAtor = loadImage("material/ator-1.png");
-    imagemDoCarro1 = loadImage("material/carro-1.png");
-    imagemDoCarro2 = loadImage("material/carro-2.png");
-    imagemDoCarro3 = loadImage("material/carro-3.png");
+    imagemDaEstrada = loadImage("material/imagens/estrada.png");
+    imagemDoAtor = loadImage("material/imagens/ator-1.png");
+    imagemDoCarro1 = loadImage("material/imagens/carro-1.png");
+    imagemDoCarro2 = loadImage("material/imagens/carro-2.png");
+    imagemDoCarro3 = loadImage("material/imagens/carro-3.png");
     imagemCarros = [imagemDoCarro1, imagemDoCarro2, imagemDoCarro3, imagemDoCarro1, imagemDoCarro2, imagemDoCarro3];
 }  
