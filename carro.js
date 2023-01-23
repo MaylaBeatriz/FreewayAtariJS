d8b785503c854eedb5f0229d7f1fb7d2d458c4c0
Mayla Rocha <maylaetb@gmail.com>
2023-01-23 11:13:26 -0300

    criacao for no mostraCarro

--- carro.js
+++ carro.js
@@ -1,25 +1,18 @@
-//carro
+//posicao do carro
 let xCarro = [700, 10, 800];
 let yCarro = [40, 96, 150];
+
+//tamanho do carro
+let larguraCarro = 70;
+let alturaCarro = 40;
+
+//velocidade do carro
 let velocidadeCarro = [2, 2.5, 3.2];
 
-//carro 1
-let larguraCarro1 = 70;
-let alturaCarro1 = 40;
-
-//carro 2
-let larguraCarro2 = 70;
-let alturaCarro2 = 40;
-
-//carro 3
-let larguraCarro3 = 70;
-let alturaCarro3 = 40;
-
-
 function mostraCarro(){
-    image(imagemCarros[0], xCarro[0], yCarro[0], larguraCarro1, alturaCarro1);
-    image(imagemCarros[1], xCarro[1], yCarro[1], larguraCarro2, alturaCarro2);
-    image(imagemCarros[2], xCarro[2], yCarro[2], larguraCarro3, alturaCarro3);
+    for (let i = 0; i < imagemCarros.length; i = i + 1) {
+        image(imagemCarros[i], xCarro[i], yCarro[i], larguraCarro, alturaCarro);
+    }
 }  
   
 function movimentaCarro(){
