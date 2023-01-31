3a64bc605d718047408b172a2a89342e38335044
Mayla Rocha <maylaetb@gmail.com>
2023-01-31 11:26:43 -0300

    correcao de bug do som

--- imagens.js
+++ imagens.js
@@ -1,3 +1,7 @@
+let somDaTrilha;
+let somDaColisao;
+let somDoPonto;
+
 let imagemDaEstrada;
 let imagemDoAtor;
 let imagemCarros;
@@ -6,10 +10,14 @@ let imagemDoCarro2;
 let imagemDoCarro3;
 
 function preload() {
-    imagemDaEstrada = loadImage("material/imagens/estrada.png");
-    imagemDoAtor = loadImage("material/imagens/ator-1.png");
-    imagemDoCarro1 = loadImage("material/imagens/carro-1.png");
-    imagemDoCarro2 = loadImage("material/imagens/carro-2.png");
-    imagemDoCarro3 = loadImage("material/imagens/carro-3.png");
+    somDaTrilha = loadSound("material/trilha.mp3");
+    somDaColisao = loadSound("material/colidiu.mp3");
+    somDoPonto = loadSound("material/pontos.wav");
+
+    imagemDaEstrada = loadImage("material/estrada.png");
+    imagemDoAtor = loadImage("material/ator-1.png");
+    imagemDoCarro1 = loadImage("material/carro-1.png");
+    imagemDoCarro2 = loadImage("material/carro-2.png");
+    imagemDoCarro3 = loadImage("material/carro-3.png");
     imagemCarros = [imagemDoCarro1, imagemDoCarro2, imagemDoCarro3, imagemDoCarro1, imagemDoCarro2, imagemDoCarro3];
 }  
--- sons.js
+++
@@ -1,9 +0,0 @@
-let somDaTrilha;
-let somDaColisao;
-let somDoPonto;
-
-function preload() {
-    somDaTrilha = loadSound("material/sons/trilha.mp3");
-    somDaColisao = loadSound("material/sons/colidiu.mp3");
-    somDoPonto = loadSound("material/sons/pontos.wav");
-}
